99f59cc30a201f128ee74715d917dd8626d23a91
manumxrtin <101816968+manumxrtin@users.noreply.github.com>
2022-04-12 18:09:47 +0200

    Adaptación a lector.h
    Eliminación del registro creado y adaptacion de todas las variables al lector.h

--- u_login.c
+++ u_login.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "lector.h"
 
+<<<<<<< Updated upstream
 typedef struct{
     int id_usuario//3+1
     char nomb_usuario[21] //20+1
@@ -31,6 +32,24 @@ void login(){
         }
     fflush(stdin);
     }while(browser==num_us); //inicio usuario
+=======
+void login(){ 
+    char nU[6];
+    char cU[9];
+    int browser;
+    Usuario *u;
+    int numUs = CargarUsuarios(&u);
+
+    do{
+    printf("Introduzca su nombre de usuario\n");
+    fgets(nU,6,stdin);
+        for(browser=0;(strcmp(nU,u[browser].nombreUsuario))!=0 && browser<numUs ;browser++){ //obtención posición 
+            if(browser==numUs) //volver a intoduzca nombre de usuario
+                printf("Usuario no encontrado, intentelo de nuevo.\n");    
+        }
+    fflush(stdin);
+    }while(browser==numUs); //inicio usuario
+>>>>>>> Stashed changes
 
     do{
     printf("Introduzca su contraseña\n");
@@ -38,16 +57,25 @@ void login(){
     if(strcmp(c_u,u[browser].contraseña)!=0)
         printf("\n Contraseña incorrecta, intentelo de nuevo.\n");
     fflush(stdin);
+<<<<<<< Updated upstream
     }while(strcmp(c_u,u[browser].contraseña)==0); //comprobación contraseña
+=======
+    }while(strcmp(cU,u[browser].contraseña)==0); //comprobación contraseña
+>>>>>>> Stashed changes
 } //inicialización de usuario
  
 void moddif_u(){ //Modificación de datos 
     int n,i;
     char d,aux;
     char pasw[9];
+<<<<<<< Updated upstream
     usuarios *u;
     int num_us;
     num_us = CargarUsuarios(*u);
+=======
+    Usuario *u;
+    int numUs = CargarUsuarios(&u);
+>>>>>>> Stashed changes
     printf("Introduzca la Id del usuario que quiere modificar\n");
     scanf("%i",&n); //Recoge los datos de busqueda del usuario concreto al que se le quiere modificar los datos
 
@@ -60,11 +88,19 @@ void moddif_u(){ //Modificación de datos
         getchar(d);
         switch(d){
             case 'N': //Modificación del nombre
+<<<<<<< Updated upstream
                 u[i].nomb_usuario=mod_name();
                 break;
 
             case 'n': //Modificación del nombre de usuario
                 u[i].nicknamw=mod_nickname();
+=======
+                u[i].nombre=mod_name();
+                break;
+
+            case 'n': //Modificación del nombre de usuario
+                u[i].nombreUsuario=mod_nickname();
+>>>>>>> Stashed changes
                 break;
 
             case 'C': //Modificación de contraseña
@@ -83,7 +119,11 @@ void moddif_u(){ //Modificación de datos
                 break;
 
             case 'P'://Modificación del perfil
+<<<<<<< Updated upstream
                 u[i].p_usuario=mod_profile();
+=======
+                u[i].perfil=mod_profile(u[i].perfil);
+>>>>>>> Stashed changes
                 break;
 
             case '0':
@@ -120,6 +160,7 @@ char *mod_pasword(){
     return(n_pasword);
 }
 
+<<<<<<< Updated upstream
 int mod_profile(){
     int n_p;
     if(usuarios.p_usuario==0){
@@ -130,6 +171,18 @@ int mod_profile(){
         usuarios.p_usuario=0;
         printf("El administrador ahora es profesor.\n");
     }
+=======
+int mod_profile(int p){
+    if(p==0){
+        p=1;
+        printf("El profesor ahora es administrador.\n");
+    }
+     if(p==1){
+        p=0;
+        printf("El administrador ahora es profesor.\n");
+    }
+    return(p);
+>>>>>>> Stashed changes
 }
 
 //f_borrar?
@@ -137,11 +190,19 @@ int mod_profile(){
 //Funcion de prueba de  las funciones
 
 void prueba_flogin(){
+<<<<<<< Updated upstream
     int num_us, auxp;
     usuarios *u,*uaux;
     num_us = CargarUsuarios(*u);
     auxp = CargarUsuarios(*uaux);
     for(auxp=0;auxp!=num_us;auxp++){
+=======
+    int numUs, auxp;
+    Usuario *u,*uaux;
+    numUs = CargarUsuarios(&u);
+    auxp = CargarUsuarios(&uaux);
+    for(auxp=0;auxp!=numUs;auxp++){
+>>>>>>> Stashed changes
         autologin(auxp);
     }
 }
@@ -149,6 +210,7 @@ void prueba_flogin(){
     //Prueba automatica flogin
     void autologin(int aux){ 
         int browser;
+<<<<<<< Updated upstream
         usuarios *u;
         int num_us;
         num_us = CargarUsuarios(*u);
@@ -159,6 +221,17 @@ void prueba_flogin(){
                     printf("Fallo en la función\n");    
             }
         }while(browser==num_us); //inicio usuario
+=======
+        Usuario *u;
+        int numUs = CargarUsuarios(&u);
+
+        do{
+            for(browser=0;(strcmp(u[aux].nombreUsusario,u[browser].nombreUsuario))!=0 && browser<numUs ;browser++){ //obtención posición 
+                if(browser==numUs) //volver a intoduzca nombre de usuario
+                    printf("Fallo en la función\n");    
+            }
+        }while(browser==numUs); //inicio usuario
+>>>>>>> Stashed changes
 
         do{
         if(strcmp(u[aux].contraseña,u[browser].contraseña)!=0)
